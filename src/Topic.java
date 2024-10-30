574d59716e954ca00aa67500491053331b1c173c
Egor <egorguk2005@outlook.com>
2024-10-30 17:07:16 +0500

    Small changes

--- src/Topic.java
+++ src/Topic.java
@@ -1,16 +1,1 @@
-public class Topic {
-    private String topicName;     // Название темы
-    private String description;   // Описание темы
-
-    public Topic(String topicName, String description) {
-        this.topicName = topicName;
-        this.description = description;
-    }
-
-    // Геттеры и сеттеры
-    public String getTopicName() { return topicName; }
-    public void setTopicName(String topicName) { this.topicName = topicName; }
-
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-}
+public record Topic(String topicName, String description) {}
